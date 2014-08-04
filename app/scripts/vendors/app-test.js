54dc935dc5ae878d6a1a7026cdd69c9ef5a4a3ff
Andrea Reginato <andrea.reginato@gmail.com>
2014-08-04 19:25:01 +0200

    Fixed some element positioning

--- app/scripts/vendors/app-test.js
+++ app/scripts/vendors/app-test.js
@@ -6,7 +6,7 @@ app.run(function($httpBackend, $timeout, Profile) {
   jasmine.getFixtures().fixturesPath = 'spec/fixtures';
 
   // set the user as logged
-  Profile.set({id: '2'});
+  Profile.set({id: '1'});
 
   // stub requests
   type = JSON.parse(readFixtures('type.json'));
